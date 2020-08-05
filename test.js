7959cd0b7dc8e8bb40ba4b93cbe4a7df070ac6b7
RozerinB <63159237+RozerinB@users.noreply.github.com>
2020-08-05 21:40:00 +0100

    Implemented D3 in external JS file

--- test.js
+++ test.js
@@ -1,22 +1,26 @@
-(function () {
-    var data = ["#039BE5", "#00897B"];
+var svg = d3.select("svg").attr("width",960).style("height",600);
+    var mycircle = svg.append("circle")
+        .attr("cx",100)
+        .attr("cy",100)
+        .style("fill","blue")
+        .attr("r",20)
 
-    d3.select("#exit")
-        .select(".apply")
-        .on("click", function () {
-            var div = d3.select("#exit")
-                .select(".selection")
-                .selectAll(".item")
-                .data(data);
+    mycircle  // wait 2 seconds, then slowly change the circle's properties
+        .transition()
+        .duration(3000)
+        .delay(2000)
+        .attr("cx",600)
+        .attr("cy",150)
+        .style("fill","orange")
+        .attr("r",50);
 
-            div.exit()
-                .remove();
-
-            div
+    mycircle  // click the circle to set it properties at random
+        .on("click",function(){
+            d3.select(this)
                 .transition()
-                .duration(500)
-                .style("background-color", function (d) {
-                    return d;
-                });
+                .duration(3000)
+                .attr("cx",Math.random()*700)
+                .attr("cy",Math.random()*300)
+                .style("fill",d3.rgb( Math.random()*255,Math.random()*255,Math.random()*255 ))
+                .attr("r",Math.random()*100);
         });
-})();
