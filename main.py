f223502efe19eec90b3025cc6497483bcf1f9dfe
Haris Nazir <haris.nazir1963@gmail.com>
2020-08-14 21:50:42 +0100

    Updated New Layout with working D3 JS

--- main.py
+++ main.py
@@ -1,77 +1,56 @@
-import numpy as np #Math Library
-import pandas as pd #Data Processing Library
-import matplotlib.pyplot as plt #Data Visualisation Library
-from mpld3.display import display_d3
-import seaborn as sns #Additional Data Visualisation Library
-from sklearn.cluster import KMeans #Clustering Library
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import plotly as py
+import plotly.graph_objs as go
+from sklearn.cluster import KMeans
+import warnings
+import os
+print("Not Done")
 
-dataset = pd.read_csv("data/Mall_Customers.csv") #Read data from CSV Files
+df = pd.read_csv("data/Mall_Customers.csv")
+df.head()
 
-print("Dataset Shape: ", dataset.shape)
+df.shape
 
-print("First 10 Rows: ", dataset.head(10))
+df.describe()
 
-print("Check for missing values and other anomalies: ", dataset.info())
+df.dtypes
 
-#There should be no missing values for this example dataset
+df.isnull().sum()
 
-plt.figure(figsize=[12,10])
-sns.scatterplot(x="Age", y="Spending Score (1-100)", data=dataset)
+X1 = df[['Age' , 'Spending Score (1-100)']].iloc[: , :].values
+inertia = []
+for n in range(1 , 11):
+    algorithm = (KMeans(n_clusters = n ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+    algorithm.fit(X1)
+    inertia.append(algorithm.inertia_)
 
-#For this example dataset we only want to consider Annual Income and Spending Score
+algorithm = (KMeans(n_clusters = 4 ,init='k-means++', n_init = 10 ,max_iter=300, 
+                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+algorithm.fit(X1)
+labels1 = algorithm.labels_
+centroids1 = algorithm.cluster_centers_
 
-X = dataset.iloc[:, [2,4]].values #iloc allows for access to data in the dataframe though aboslute position in the form of [[rows], [columns]]
-print(X)
+h = 0.02
+x_min, x_max = X1[:, 0].min() - 1, X1[:, 0].max() + 1
+y_min, y_max = X1[:, 1].min() - 1, X1[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()]) 
 
-#To use the K means algorithm we need to decide on the number of clusters needed this will be done below:
-inertias=[]
+plt.figure(1 , figsize = (15 , 7) )
+plt.clf()
+Z = Z.reshape(xx.shape)
+plt.imshow(Z , interpolation='nearest', 
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap = plt.cm.Pastel2, aspect = 'auto', origin='lower')
 
-for i in range(1,11):
-    
-    #init = "Kmeans++" which selects the inital cluster center for K-Means clustering in a smarter way in order to speed up convergence
-    kmeans = KMeans(n_clusters=i)
+plt.scatter( x = 'Age' ,y = 'Spending Score (1-100)' , data = df , c = labels1 , 
+            s = 200 )
+plt.scatter(x = centroids1[: , 0] , y =  centroids1[: , 1] , s = 300 , c = 'red' , alpha = 0.5)
+plt.ylabel('Spending Score (1-100)') , plt.xlabel('Age')
+plt.show()
 
-    #fit is the method that will do the convergence on the position of the centroids
-    kmeans.fit(X)
-
-    #ineratia_ is a mesaure of how good the fit is (the lower the better)
-    inertias.append(kmeans.inertia_)
-
-#visualise interatia vs k (the num of clusters) & using elbow method decide on optimal value of K
-plt.figure(figsize=[12,10])
-p=sns.lineplot(range(1,11),inertias)
-p.set_title('The Elbow Method')
-dummy = p.set(xlabel='no of clusters', ylabel='inertia')
-
-#elbow comes at k=4, so we can build the model with k = 5
-kmeansmodel = KMeans(n_clusters = 4)
-#find position of centroids
-kmeansmodel.fit(X)
-#Assigns each data point to a cluster
-clustered = kmeansmodel.predict(X)
-
-print(clustered)
-
-plt.figure(figsize=[12,10])
-
-# X has 2 coloums: 0 and 1
-# X[clustered == n, 0] gives you the 'x' points (annual income)
-# X[clustered == n, 1] gives you the 'y' points (spending scores)
-
-#We do this for each of the 4 clusters
-#s is the point radius
-
-sns.scatterplot(X[clustered == 0, 0], X[clustered == 0, 1], s = 80, label = "Cluster 1")
-sns.scatterplot(X[clustered == 1, 0], X[clustered == 1, 1], s = 80, label = "Cluster 2")
-sns.scatterplot(X[clustered == 2, 0], X[clustered == 2, 1], s = 80, label = "Cluster 3")
-sns.scatterplot(X[clustered == 3, 0], X[clustered == 3, 1], s = 80, label = "Cluster 4")
-
-#then the centroids
-sns.scatterplot(kmeans.cluster_centers_[:, 0], kmeansmodel.cluster_centers_[:, 1], s = 1000, color = "black", marker="+", label = "Centroids")
-
-plt.title("Customer Segmentation")
-plt.xlabel("Annual Income ($)")
-plt.ylabel("Spending Score (1-100)")
-plt.legend()
-
-display_d3(plt.show())
+print("Done")
