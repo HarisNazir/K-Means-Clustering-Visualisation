b46fccf9264f7186eed8905d7a8a460fa732f4d5
Haris Nazir <haris.nazir1963@gmail.com>
2020-08-30 15:03:35 +0100

    CSV and New scatter

--- k-means.js
+++ k-means.js
@@ -1,315 +1,72 @@
-var flag = false;
-var WIDTH = d3.select("#kmeans")[0][0].offsetWidth - 20;
-var HEIGHT = Math.max(300, WIDTH * .7);
-var manualPlacement = false;
-var placingFinished = false;
-var drawCentroids = false;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 40, left: 50},
+    width = 520 - margin.left - margin.right,
+    height = 520 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var Svg = d3.select("#my_dataviz2")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")")
 
 
-d3.csv("data/Mall_Customers.csv", function(data){
-    for (var i = 0; i < data.length; i++){
-        console.log("CustomerID: " + data[i].CustomerID);
-        console.log("Gender: " + data[i].Gender);
-        console.log("Age: " + data[i].Age);
-        console.log("Annual Income (k$): " + data[i].Annual_Income);
-        console.log("Spending Score (1-100): " + data[i].Spending_Score);
-    }
-});
 
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) {
 
-var svg = d3.select("#kmeans svg")
-.attr('width', WIDTH)
-.attr('height', HEIGHT)
-.style('padding', '10px')
-.style('background', '#ADB5BD') //change background colour 
-.style('cursor', 'pointer')
-.style('-webkit-user-select', 'none')
-.style('-khtml-user-select', 'none')
-.style('-moz-user-select', 'none')
-.style('-ms-user-select', 'none')
-.style('user-select', 'none')
-.on('click', function() {
-    d3.event.preventDefault();
-    if (!manualPlacement || placingFinished) {
-        step();
-    }
-});
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([4*0.95, 8*1.001])
+    .range([ 0, width ])
+  Svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(-height*1.3).ticks(10))
+    .select(".domain").remove()
 
-d3.selectAll("#kmeans button")
-.style('padding', '.5em .8em');
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([-0.001, 9*1.01])
+    .range([ height, 0])
+    .nice()
+  Svg.append("g")
+    .call(d3.axisLeft(y).tickSize(-width*1.3).ticks(7))
+    .select(".domain").remove()
 
-d3.selectAll("#kmeans label")
-.style('display', 'inline-block')
-.style('width', '15em');
+  // Customization
+  Svg.selectAll(".tick line").attr("stroke", "#EBEBEB")
 
-var lineg = svg.append('g');
-var dotg = svg.append('g');
-var centerg = svg.append('g');
-d3
+  // Add X axis label:
+  Svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("x", width)
+      .attr("y", height + margin.top + 20)
+      .text("Sepal Length");
 
-d3.select("#step")
-.on('click', function() { 
-    step(); draw(); 
-});
-d3.select("#run").
-on('click', function() { 
-    startRun(); 
-});
-d3.select("#restart")
-.on('click', function() { 
-    restart(); draw(); 
-});
-d3.select("#reset")
-.on('click', function() { 
-    init(); draw(); 
-});
+  // Y axis label:
+  Svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left+20)
+      .attr("x", -margin.top)
+      .text("Petal Length")
 
-var groups_old = [];
-var groups = [], dots = [];
-var firstRun = true;
-var inter;
+  // Color scale: give me a specie name, I return a color
+  var color = d3.scaleOrdinal()
+    .domain(["setosa", "versicolor", "virginica" ])
+    .range([ "#402D54", "#D18975", "#8FD175"])
 
-function startRun() {
-    $("#run").prop("disabled", true);
-    $("#step").prop("disabled", true);
-    step();
-    inter = setInterval(step, 750);
-}
-function centroids(cb) {
-    drawCentroids = (cb.checked);
-}
-function manual(cb) {
-    manualPlacement = (cb.checked);
-    init();
-}
-function step() {
-    d3.select("#restart").attr("disabled", null);
-    if (flag) {
-        moveCenter();
-        draw();
-    } else {
-        updateGroups();
-        draw();
-    }
-    flag = !flag;
-}
-function place(i, K, x1, y1) {
-    var g = {
-        dots: [],
-        color: 'hsl(' + (i * 360 / K) + ',100%,50%)',
-        center: {
-            x: x1,
-            y: y1 
-        },
-        init: {
-                  center: {}
-              }
-    };
-    g.init.center = {
-        x: g.center.x,
-        y: g.center.y
-    };
-    return g;
-}
-var placed = 0;
-function init() {
-    $("#step").prop("disabled", manualPlacement);
-    $("#run").prop("disabled", manualPlacement);
-    clearInterval(inter);
-    firstRun = true;
-    placed = 0;
-    d3.select("#restart").attr("disabled", "disabled");
+  // Add dots
+  Svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.Sepal_Length); } )
+      .attr("cy", function (d) { return y(d.Petal_Length); } )
+      .attr("r", 5)
+      .style("fill", function (d) { return color(d.Species) } )
 
-    var N = parseInt(d3.select('#N')[0][0].value, 10);
-    var K = parseInt(d3.select('#K')[0][0].value, 10);
-    groups = [];
-
-    if (!manualPlacement) {
-        for (var i = 0; i < K; i++) {
-            groups.push(place(i, K, Math.random() * WIDTH, Math.random() * HEIGHT));
-        }
-    } else {
-        d3.select("#kmeans svg").on("click", function() {
-            if (placed < K && manualPlacement) {
-                var coords = d3.mouse(this);
-                groups.push(place(placed, K, coords[0], coords[1]));
-                draw();
-                placed++;
-                if (placed == (K)) {
-                    $("#run").prop("disabled", false);
-                    $("#step").prop("disabled", false);
-                }
-            } else {
-                step();
-                draw();
-            }
-        });
-    }
-    dots = [];
-    flag = false;
-    dots = drawCentroids ? pushCentroids(N, K) : pushRands(N) 
-    draw();
-}
-function pushRands(N) {
-    dots = []
-    for (var i = 0; i < N; i++) {
-            var dot = {
-                x: Math.random() * WIDTH,
-                y: Math.random() * HEIGHT,
-                group: undefined
-            };
-            dot.init = {
-                x: dot.x,
-                y: dot.y,
-                group: dot.group
-            };
-            dots.push(dot);
-        }
-    return dots;
-}
-
-function pushCentroids(N, K) {
-    dots = [];
-    for (var i = 0; i < K; i++) {
-        var cX = Math.random() * WIDTH;
-        var cY = Math.random() * HEIGHT;
-        var cW = getRandomArbitrary(50,125);
-        var cH = getRandomArbitrary(50,125);
-        for (var j = 0; j < N/K; j++) {
-                var rX = Math.random() * cW;
-                var x = cX + ((cX + rX < WIDTH) ? rX : -1 * rX);
-                var rY = Math.random() * cH;
-                var y = cY + ((cY + rY < HEIGHT) ? rY : -1 * rY);
-            var dot = {
-                x: x,/*(Math.random() * WIDTH/K) + cX,*/
-                y: y, /*(Math.random() * HEIGHT/K) + cY,*/
-                group: undefined
-            };
-            dot.init = {
-                x: dot.x,
-                y: dot.y,
-                group: dot.group
-            };
-            dots.push(dot);
-        }
-    }
-    return dots;
-}
-/* from Mozilla Developer Center */
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-function restart() {
-    $("#step").prop("disabled", false);
-    $("#run").prop("disabled", false);
-    clearInterval(inter);
-    firstRun = true;
-    flag = false;
-    d3.select("#restart").attr("disabled", "disabled");
-    groups.forEach(function(g) {
-        g.dots = [];
-        g.center.x = g.init.center.x;
-        g.center.y = g.init.center.y;
-    });
-
-    for (var i = 0; i < dots.length; i++) {
-        var dot = dots[i];
-        dots[i] = {
-            x: dot.init.x,
-            y: dot.init.y,
-            group: undefined,
-            init: dot.init
-        };
-    }
-}
-
-
-function draw() {
-    var circles = dotg.selectAll('circle')
-        .data(dots);
-    circles.enter()
-        .append('circle');
-    circles.exit().remove();
-    circles
-        .transition()
-        .duration(500)
-        .attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; })
-        .attr('fill', function(d) { return d.group ? d.group.color : '#ffffff'; }) // change colour of dots 
-        .attr('r', 5);
-
-    if (dots[0].group) {
-        var l = lineg.selectAll('line')
-            .data(dots);
-        var updateLine = function(lines) {
-            lines
-                .attr('x1', function(d) { return d.x; })
-                .attr('y1', function(d) { return d.y; })
-                .attr('x2', function(d) { return d.group.center.x; })
-                .attr('y2', function(d) { return d.group.center.y; })
-                .attr('stroke', function(d) { return d.group.color; });
-        };
-        updateLine(l.enter().append('line'));
-        updateLine(l.transition().duration(500));
-        l.exit().remove();
-    } else {
-        lineg.selectAll('line').remove();
-    }
-
-    var c = centerg.selectAll('path')
-        .data(groups);
-    var updateCenters = function(centers) {
-        centers
-            .attr('transform', function(d) { return "translate(" + d.center.x + "," + d.center.y + ") rotate(45)";})
-            .attr('fill', function(d,i) { return d.color; })
-            .attr('stroke', '#aabbcc');
-    };
-    c.exit().remove();
-    updateCenters(c.enter()
-            .append('path')
-            .attr('d', d3.svg.symbol().type('cross'))
-            .attr('stroke', '#aabbcc'));
-    updateCenters(c
-            .transition()
-            .duration(500));}
-
-    function moveCenter() {
-        groups.forEach(function(group, i) {
-            if (group.dots.length == 0) return;
-
-            // get center of gravity
-            var x = 0, y = 0;
-            group.dots.forEach(function(dot) {
-                x += dot.x;
-                y += dot.y;
-            });
-
-            group.center = {
-                x: x / group.dots.length,
-            y: y / group.dots.length
-            };
-        });
-
-    }
-
-function updateGroups() {
-    groups.forEach(function(g) { g.dots = []; });
-    dots.forEach(function(dot) {
-        // find the nearest group
-        var min = Infinity;
-        var group;
-        groups.forEach(function(g) {
-            var d = Math.pow(g.center.x - dot.x, 2) + Math.pow(g.center.y - dot.y, 2);
-            if (d < min) {
-                min = d;
-                group = g;
-            }
-        });
-
-        // update group
-        group.dots.push(dot);
-        dot.group = group;
-    });
-}
-
-init(); draw();
+})
