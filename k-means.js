2d641815d0bcba7532d8687a53304896fd93a7bc
Haris Nazir <haris.nazir1963@gmail.com>
2020-08-30 16:47:19 +0100

    New Layout and Scatterplot

--- k-means.js
+++ k-means.js
@@ -1,72 +1,59 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 40, left: 50},
-    width = 520 - margin.left - margin.right,
-    height = 520 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var Svg = d3.select("#my_dataviz2")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")")
-
-
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+    // append the svg object to the body of the page
+    var svg = d3.select("#my_dataviz")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")")
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) {
+d3.csv("data/Mall_Customers.csv", function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([4*0.95, 8*1.001])
-    .range([ 0, width ])
-  Svg.append("g")
+    .domain([0, 0])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("class", "myXaxis")   // Note that here we give a class to the X axis, to be able to call it later and modify it
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(-height*1.3).ticks(10))
-    .select(".domain").remove()
+    .call(d3.axisBottom(x))
+    .attr("opacity", "0")
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([-0.001, 9*1.01])
-    .range([ height, 0])
-    .nice()
-  Svg.append("g")
-    .call(d3.axisLeft(y).tickSize(-width*1.3).ticks(7))
-    .select(".domain").remove()
-
-  // Customization
-  Svg.selectAll(".tick line").attr("stroke", "#EBEBEB")
-
-  // Add X axis label:
-  Svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("x", width)
-      .attr("y", height + margin.top + 20)
-      .text("Sepal Length");
-
-  // Y axis label:
-  Svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.left+20)
-      .attr("x", -margin.top)
-      .text("Petal Length")
-
-  // Color scale: give me a specie name, I return a color
-  var color = d3.scaleOrdinal()
-    .domain(["setosa", "versicolor", "virginica" ])
-    .range([ "#402D54", "#D18975", "#8FD175"])
+    .domain([0, 100])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
   // Add dots
-  Svg.append('g')
+  svg.append('g')
     .selectAll("dot")
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d.Sepal_Length); } )
-      .attr("cy", function (d) { return y(d.Petal_Length); } )
-      .attr("r", 5)
-      .style("fill", function (d) { return color(d.Species) } )
+      .attr("cx", function (d) { return x(d.Age); } )
+      .attr("cy", function (d) { return y(d.Spending_Score); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
 
+  // new X axis
+  x.domain([0, 100])
+  svg.select(".myXaxis")
+    .transition()
+    .duration(2000)
+    .attr("opacity", "1")
+    .call(d3.axisBottom(x));
+
+  svg.selectAll("circle")
+    .transition()
+    .delay(function(d,i){return(i*3)})
+    .duration(2000)
+    .attr("cx", function (d) { return x(d.Age); } )
+    .attr("cy", function (d) { return y(d.Spending_Score); } )
 })
